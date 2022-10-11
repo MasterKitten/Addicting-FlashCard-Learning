13a1ff3a4839442a6c9336687de7ec9ff590f606
MasterKitten <51054796+MasterKitten@users.noreply.github.com>
2022-10-10 19:02:13 -0700

    Small update
    Small update for new build

--- Schodot/Extra Scenes/HelpMe.gd
+++ Schodot/Extra Scenes/HelpMe.gd
@@ -2,18 +2,21 @@ extends ColorRect
 
 export (String) var SelectedAnswer
 export (String) var SelectedQuestion
-var QuestionCommands = ["Testing1", "Testing2", "Testing3"]
-var AnswerCommands = ["Toosting1", "Toosing2", "Toosting3"]
+var QuestionCommands = ["Look up to the sky and say it!", "What does this mean!?", "Look to the ground and say this!", 
+"Look to your right and say it!", "Imagine a goldfish saying this word!", "Say this out loud!", "Write this down 3 times!", 
+"Write this in BOLD"]
+var AnswerCommands = ["What does this mean!?", "Look to your right and say this!", "Point to a object and say it!", 
+"Bounce and say this once!", "Look at the time and say this!", "Write this down 3 times!", "Imagine this word!", "Write this in Italics!"]
 
 # Do you want to memorize!!?? DO YOU WANT TO MEMORIZE!!???
-export (int) var AmountOfSaying = 5
+export (int) var AmountOfSaying = 6
 var i = 0
 
 var Music = AudioServer.get_bus_index("Music")
 var AudioLevel = db2linear(AudioServer.get_bus_volume_db(Music))
 
 func Commanding():
-	AudioServer.set_bus_volume_db(Music, linear2db(-0.25))
+	AudioServer.set_bus_volume_db(Music, linear2db(0.20))
 	var RNG = RandomNumberGenerator.new()
 	RNG.randomize()
 	if RNG.randi_range(0, 1) == 1:
