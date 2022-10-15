ef7074bc9ea7a52da06460db1057153c615f94e0
MasterKitten <51054796+MasterKitten@users.noreply.github.com>
2022-10-15 10:52:12 -0700

    Added ending scene in Word mode

--- Schodot/Scenes/Game Modes/Word Game.gd
+++ Schodot/Scenes/Game Modes/Word Game.gd
@@ -9,11 +9,16 @@ onready var SelectingPrac = get_parent().get_parent().get_node("Selecting Practi
 
 export (PackedScene) var Butt
 
+var Correct = 0
+var Wrong = 0
+var Total = 0
+var Dumb = false
+
 func Answer(Number):
 	if Number == RandomQuestion:
-		print("Correct")
+		Correct += 1
 	else:
-		print("Wrong")
+		Wrong += 1
 	SelectedQuestions.remove(RandomQuestion)
 	SelectedAnswers.remove(RandomQuestion)
 	var Children = get_node("ScrollContainer/Container").get_children()
@@ -22,12 +27,14 @@ func Answer(Number):
 		Children[i].queue_free()
 		i += 1
 	if SelectedAnswers.size() == 0:
-		SelectingPrac.BackToLevel()
-		queue_free()
+		Finality()
 	else:
 		UpdateText()
 
 func UpdateText():
+	if Dumb == false:
+		Total = SelectedAnswers.size()
+		Dumb = true
 	var i = 0
 	while i < SelectedAnswers.size():
 		var Item = Butt.instance()
@@ -39,3 +46,13 @@ func UpdateText():
 	RNG.randomize()
 	RandomQuestion = RNG.randi_range(0, SelectedQuestions.size() - 1)
 	get_node("TextureRect/RichTextLabel").text = SelectedQuestions[RandomQuestion]
+
+func Finality():
+	get_node("Results/Correct").text = "Correct: " + str(Correct)
+	get_node("Results/Wrong").text = "Wrong: " + str(Wrong)
+	get_node("Results/Progress2").text = str(Correct) + " / " + str(Total)
+	get_node("Results").visible = true
+
+func _on_Continue_pressed():
+	SelectingPrac.BackToLevel()
+	queue_free()
